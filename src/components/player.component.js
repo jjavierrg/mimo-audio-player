7381151d8e5a6eaaa30a0fbfabd3d7b950351e65
José Javier Rodríguez Gallego <jjavierrg@gmail.com>
2021-02-06 22:52:17 +0100

    Añadido indicador de progreso

--- src/components/player.component.js
+++ src/components/player.component.js
@@ -3,29 +3,40 @@ export default {
     props: ["track"],
     template: `
         <div class="d-flex align-items-center c-pointer bg-dark text-light py-2 px-3">
-            <div v-if ="!playing" title="play" @click="unpause" class="me-3"><i class="fas fa-2x fa-play"></i></div>
+            <div v-if ="!playing" title="play" @click="play" class="me-3"><i class="fas fa-2x fa-play"></i></div>
             <div v-if ="playing" title="pause" @click="pause" class="me-3"><i class="fas fa-2x fa-pause"></i></div>
             <div v-if ="!muted" title="click to mute" @click="mute" class="me-3"><i class="fas fa-2x fa-volume-up"></i></div>
             <div v-if ="muted" title="click to unmute" @click="unmute" class="me-3"><i class="fas fa-2x fa-volume-mute"></i></div>
             <img  v-if="track && track.image" v-bind:src="track.image" class="me-2 img-album-thumb" /> 
-            <div v-if="track" class="d-flex flex-grow-1 no-selection flex-column">
-                <span >{{track.artist_name}} - {{track.name}}</span>
-                <span >{{track.album_name}}</span>
+            <div v-if="track" class="d-flex flex-grow-1 no-selection flex-column me-2">
+                <span >{{track.artist_name}} - {{track.name}} ({{track.album_name}})</span>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" v-bind:style="{ width: progess + '%' }" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
             </div>
             <div v-if="track && track.audiodownload_allowed" title="Click to download"><i class="fas fa-2x fa-download"></div>
         </div>
     `,
     created() {
         this.audio = new Audio();
+        this.audio.ontimeupdate = () => {
+            const progress = this.audio.currentTime * 100 / this.audio.duration;
+            this.progess = Math.min(100, progress)
+        };
+        this.audio.onended = () => {
+            this.reset();
+            this.track = null;
+        };
     },
     data() {
         return {
             playing: false,
             muted: false,
+            progess: 0,
         };
     },
     methods: {
-        unpause() {
+        play() {
             this.playing = true;
             this.audio.play();
         },
@@ -41,6 +52,10 @@ export default {
             this.muted = false;
             this.audio.muted = false;
         },
+        reset() {
+            this.audio.pause();
+            this.progess = 0;
+        }
     },
     watch: {
         track: function (track, oldTrack) {
@@ -48,11 +63,9 @@ export default {
                 return;
             }
 
-            this.audio.pause();
-            const url = track.audio;
-            this.audio.src = url;
-            this.audio.play();
-            this.playing = true;
+            this.reset();
+            this.audio.src = track.audio;
+            this.play();
         }
     }
 }
